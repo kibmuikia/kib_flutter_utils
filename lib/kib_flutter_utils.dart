00034143130caa6765fc68946f135b17028e899d
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2025-05-16 23:17:34 +0300

    task: lib/kib_flutter_utils.dart: export the utils.

--- lib/kib_flutter_utils.dart
+++ lib/kib_flutter_utils.dart
@@ -1,5 +1,6 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library;
+
+export 'src/general_utils.dart' show postFrame, KeyboardUtil;
+export 'src/snackbar_utils.dart' show showSnackbarMessage, SnackbarExtension;
+export 'src/widget_utils.dart';
+export 'src/error_utils/exceptions.dart';
