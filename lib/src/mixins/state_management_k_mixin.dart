64d2b4e051baedbfde3fdbe669a15469181afbec
Allan Muikia <31766430+kibmuikia@users.noreply.github.com>
2025-07-28 08:22:42 +0300

    task: StateManagementK: update by: - add _statusMessage, - add getters for _statusMessage and more error handling, - add utils such as startLoading, resetAllStates, updateState & setStatusMessage.

--- lib/src/mixins/state_management_k_mixin.dart
+++ lib/src/mixins/state_management_k_mixin.dart
@@ -9,9 +9,15 @@ import 'package:kib_utils/kib_utils.dart' show Result, failure, success;
 mixin StateManagementK<T extends StatefulWidgetK> on StateK<T> {
   bool _isLoading = false;
   Object? _error;
+  String _statusMessage = "";
 
   bool get isLoading => _isLoading;
+  String get statusMessage => _statusMessage;
   Object? get error => _error;
+  Type? get errorType => _error?.runtimeType;
+  String? get genericErrorMessage =>
+      errorType == null ? null : 'Encountered $errorType issue';
+  bool get hasError => _error != null;
 
   /// Safely updates loading state
   @protected
@@ -29,14 +35,57 @@ mixin StateManagementK<T extends StatefulWidgetK> on StateK<T> {
     }
   }
 
+  /// Safely updates statusMessage state
+  @protected
+  void setStatusMessage(String message) {
+    if (mounted) {
+      setState(() => _statusMessage = message);
+    }
+  }
+
+  /// Safely updates isLoading or error or statusMessage; in any combination
+  /// Only sets a state if its not null.
+  @protected
+  void updateState({bool? isLoading, Object? error, String? statusMessage}) {
+    if (mounted) {
+      setState(() {
+        if (isLoading != null) _isLoading = isLoading;
+        if (error != null) _error = error;
+        if (statusMessage != null) _statusMessage = statusMessage;
+      });
+    }
+  }
+
+  /// Safely reset all states
+  @protected
+  void resetAllStates() {
+    if (mounted) {
+      setState(() {
+        _isLoading = false;
+        _error = null;
+        _statusMessage = "";
+      });
+    }
+  }
+
+  /// Safely start a loading state by setting isLoading to true and error to null
+  @protected
+  void startLoading() {
+    if (mounted) {
+      setState(() {
+        _isLoading = true;
+        _error = null;
+      });
+    }
+  }
+
   /// Helper to wrap async operations with loading and error states
   @protected
   Future<Result<A, Exception>> withStateManagement<A>(
     Future<A> Function() operation,
   ) async {
     try {
-      setLoading(true);
-      setError(null);
+      startLoading();
       final result = await operation();
       return success(result);
     } catch (e, stack) {
